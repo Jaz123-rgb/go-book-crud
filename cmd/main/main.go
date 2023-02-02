283f54375a605a0742789fe27044380a476a309c
Jaz <pjaziel723@gmail.com>
2023-02-01 18:17:26 -0600

    First commit

---
+++ cmd/main/main.go
@@ -0,0 +1,20 @@
+package main
+import (
+	"log"
+	"net/http"
+	"github.com/Jaz123-rgb/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+
+}
+
+
+
+
